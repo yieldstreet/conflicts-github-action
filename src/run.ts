5c2b35e7cef1d4ca1b5b48b0cdd2c2eb34eea44b
pt-br <ltavares@yieldstreet.com>
2022-05-25 17:10:50 -0300

    Test type for $label

--- src/run.ts
+++ src/run.ts
@@ -34,7 +34,7 @@ export async function run(): Promise<void> {
     }
 
     await runOnAll(octokit, github.context, conflictLabel, waitMs, maxRetries, detectMergeChanges)
-  } catch (error) {
+  } catch (error: any) {
     core.setFailed(error.message)
   }
 }
